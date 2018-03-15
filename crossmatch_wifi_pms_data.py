9a746e1423c7c815d8fbc3ab37c6f8d68399097b
katzenrummel <kevin.loska@gmail.com>
2018-03-15 12:19:54 +0000

    Update string matching logic and add threshold values

--- crossmatch_wifi_pms_data.py
+++ crossmatch_wifi_pms_data.py
@@ -1,19 +1,11 @@
 """
 crossmatch_wifi_pms_data.py
-
-Script for taking an standardized Oracle PMS export as the source input and a SQL query generated csv file consisting of names, dates, email addresses and mobile numbers as a second input file.
-The output file is a copy of the PMS data with email addresses and/or mobile numbers merged from the WiFI signup data. 
-Contact information are only merged and assumed as a match if:
- (1) Unique record in the Wifi data exists for matching only with one person (first name and last name) of the PMS data
- (2) First Wifi Session Date matches with the arrival date of the pms data
- (3) Eventually contains parts of the first name and last name in the email address
-  
-Assumption: Wifi login data ordered alphabetically 
 """
 
 import csv
 from Levenshtein import jaro_winkler
 from prettytable import PrettyTable
+import datetime
 
 # If no prefix weight is explicitly specified then 0.1 is the default
 # to consider the strings *identical*
@@ -23,8 +15,8 @@ def calculate_similarity(string_1, string_2):
 def is_empty(string):
  return not bool(string.strip()) 
 
-def write_plotting_data(data):
- with open('plotting.csv','w', newline='') as plotting_file:
+def write_data_csv(data, filename):
+ with open(filename,'w', newline='') as plotting_file:
   fieldnames = list(data[0].keys())
 
   writer = csv.DictWriter(plotting_file, fieldnames = fieldnames)
@@ -32,18 +24,28 @@ def write_plotting_data(data):
   writer.writeheader()
   writer.writerows(data)
 
+def add_timestamp():
+ return datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+
+
 # Specify input name for PMS data source
-pms_input_filename = 'SENT_PA_2702_v0.1.csv'
+pms_input_filename = 'in/SENT_PA_2702_v0.1.csv'
 
 # Specify input name for Wifi signup data
-wifi_input_filename = 'Sentosa_First_Wifi_session_data_27.02.csv'
+wifi_input_filename = 'in/Sentosa_First_Wifi_session_data_27.02.csv'
+
+# Define genuine match threshold
+match_threshold = 0.8
 
 print("Start program...")
 
 # List with similarity measures for plotting
 sim_plotting_list = []
 
-table = PrettyTable(['pms_first_name', 'pms_last_name','sim_first_name','sim_last_name','sim_firsname_to_email','sim_lastname_to_email'])
+# List with reservation records and merged email address
+merged_output = []
+
+table = PrettyTable(['pms_first_name', 'pms_last_name','sim_mean', 'name_matched'])
 
 with open(pms_input_filename, newline='') as pms_data:
  pms_file_reader = csv.DictReader(pms_data)
@@ -71,17 +73,18 @@ with open(pms_input_filename, newline='') as pms_data:
      sim_lastname_to_username = calculate_similarity(pms_last_name, wifi_email_username)
 
      sim_mean = (sim_first_name + 
-           	sim_last_name + 
-          	sim_firstname_to_username) / 3
+           	sim_last_name) / 2
 
      #print('Similarity Measure between first name {} and {} : {}'.format(pmsFirstName, wifiFirstName, simPmsXWifiFirstName))     
      #print('Similarity Measure between last name {} and {} : {}'.format(pmsLastName, wifiLastName,simPmsXWifiLastName)) 
-    # print('Similarity Measure between username {} and {} : {}'.format(pmsFirstName, wifiEmailUsername, simPmsXWifiUsername ))
+     # print('Similarity Measure between username {} and {} : {}'.format(pmsFirstName, wifiEmailUsername, simPmsXWifiUsername ))
      
-     if sim_mean >= 0.65:
-      #print('Identified record for {} with a mean similarity measure of : {}'.format(pms_first_name, sim_mean))
-      
-      table.add_row([pms_first_name, pms_last_name,sim_first_name,sim_last_name,sim_firstname_to_username,sim_lastname_to_username])      
+     if sim_mean >= match_threshold:
+      tmp_reservation = reservation
+      tmp_reservation['EMAIL '] = wifi_record['email']
+      merged_output.append(tmp_reservation)
+
+      table.add_row([pms_first_name, pms_last_name,sim_mean,'straight_match'])      
 
       # Build data for plotting
       plotting_record = {}
@@ -98,10 +101,36 @@ with open(pms_input_filename, newline='') as pms_data:
 
       sim_plotting_list.append(plotting_record)
 
+     elif sim_mean < match_threshold and sim_mean >= 0.5:
+      if sim_firstname_to_username >= 0.75 or sim_lastname_to_username >= 0.75:       
+       tmp_reservation = reservation
+       tmp_reservation['EMAIL '] = wifi_record['email']
+       merged_output.append(tmp_reservation)
+
+       table.add_row([pms_first_name, pms_last_name,sim_mean,'email_weight'])      
+
+       # Build data for plotting
+       plotting_record = {}
+       plotting_record['pms_first_name'] = pms_first_name
+       plotting_record['pms_last_name'] = pms_last_name
+       plotting_record['wifi_first_name'] = wifi_first_name
+       plotting_record['wifi_last_name'] = wifi_last_name
+       plotting_record['wifi_email_username'] = wifi_email_username
+       plotting_record['sim_first_name'] = sim_first_name
+       plotting_record['sim_last_name'] = sim_last_name
+       plotting_record['sim_first_name_to_email'] = sim_firstname_to_username
+       plotting_record['sim_last_name_to_email'] = sim_lastname_to_username
+       plotting_record['sim_mean_value'] = sim_mean
+
+       sim_plotting_list.append(plotting_record)
+
 print(table)
 
 # Write Plotting CSV
-write_plotting_data(sim_plotting_list)
+write_data_csv(sim_plotting_list,'out/plotting.csv')
+
+# Write Output CSV
+write_data_csv(merged_output,'out/merged_output{}.csv'.format(add_timestamp()))
 
 print('End of program')
 
